73ec6fea80aed23e0ddf0442695a5c8175b25bbb
jjusticz <jjusticz@uchicago.edu>
2023-02-16 05:51:39 +0000

    Fifth commit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -20,4 +20,17 @@ class PhotosController < ApplicationController
 
   end
 
+  def baii
+
+    the_id = params.fetch("toast_id")
+
+    matching_photos = Photo.where({:id => the_id})
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    redirect_to("/photos")
+  end
+
 end
